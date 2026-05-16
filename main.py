a33c0003aec5c84e046c6bf7d9106eb59d41ea47
Carlos Daniel <danieldpaula07@gmail.com>
2026-05-16 14:17:39 -0300

    feat: adiciona calculo da media das idades

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+soma_idade = 0
+
 for i in range(0,4):
 
     nome = input("Digite o nome: ")
     idade = int(input("Digite a idade: "))
     sexo = input("Digite o sexo: ").upper()
+
+    soma_idade += idade
+
+media = soma_idade / 4
+
+print(f"Media das idades: {media}")
